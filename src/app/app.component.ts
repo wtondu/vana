344b06e93fb2ca2b65db1c5b9aba13d68fcbcabc
Walter Tondu <walter.tondu@optimityadvisors.com>
2021-10-02 11:52:22 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'golf-scores';
+  url = 'https://vana-handicap-walter-tondu.azurewebsites.net/api/httptrigger2';
+  savedName: string;
+  savedScores: string;
+
+  golfForm = new FormGroup({
+    fName: new FormControl('', Validators.required),
+    scores: new FormControl('', Validators.required)
+  });
+  handicapData: any;
+
+  constructor(
+    private http: HttpClient
+  ) {}
+
+  onSubmit(): void {
+    this.getHandicap(this.golfForm.controls.fName.value, this.golfForm.controls.scores.value).subscribe(
+      (response: any) => {
+        this.handicapData = response;
+      }
+    );
+  }
+
+  public getHandicap(name: string, scores: string): Observable<any> {
+    this.savedName = name;
+    this.savedScores = scores;
+    const data = { name, scores };
+    return this.http.post(this.url, data);
+  }
 }
